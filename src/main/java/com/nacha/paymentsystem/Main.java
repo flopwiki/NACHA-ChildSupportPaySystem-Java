1011b00408b8cad535ed301f1e6660d9bf2232cd
fiopwk <da-juan3861@outlook.com>
2024-09-30 20:23:49 -0400

    Main.java: entry point

---
+++ src/main/java/com/nacha/paymentsystem/Main.java
@@ -0,0 +1,8 @@
+package com.nacha.paymentsystem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Child Support Payment System.");
+        System.out.println("Please select your state or employer to begin.");
+    }
+}
