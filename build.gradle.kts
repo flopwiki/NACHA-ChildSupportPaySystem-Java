58db6874afd53ebd052cd946103427935b4cdfda
fiopwk <da-juan3861@outlook.com>
2024-10-01 08:23:41 -0400

    security: added owaps plugin to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,20 @@ description = "nacha-paymentsystem"
 plugins {
     // Java plugin for building Java applications
     id("java")
-    
+
     // Apply application plugin if you're building an executable app
     application
+    // implementation
 }
-
 java {
     // Set Java 21 as the source and target compatibility
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
+
+
+
 
 repositories {
     // Use Maven Central repository to resolve dependencies
@@ -26,8 +29,9 @@ dependencies {
     // JUnit 5 for unit testing
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
-    
+
     // Additional dependencies can go here (e.g., logging, database, etc.)
+    implementation("org.owasp:dependency-check-gradle:10.0.2")
 }
 
 tasks.test {
