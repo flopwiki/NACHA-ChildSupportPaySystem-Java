b092ff88966f898962323a1eb472557e8468d8da
fiopwk <da-juan3861@outlook.com>
2024-10-01 07:40:55 -0400

    logger: begin logging implementation

--- src/main/java/org/nacha/paymentsystem/Main.java
+++ src/main/java/org/nacha/paymentsystem/Main.java
@@ -2,8 +2,12 @@ package org.nacha.paymentsystem;
 
 import org.nacha.paymentsystem.Welcome.Welcome;
 
+import java.util.logging.Logger;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(Welcome.getWelcomeMessage());
+        Logger logger = Logger.getLogger(Logger.getLogger("Main").getName());
+        logger.info("Initializing payment system....completed");
     }
 }
